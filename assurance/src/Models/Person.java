5b0bfea4225983cb0bce762404b4eecc2a55624a
asma828 <asmalachhab66@gmail.com>
2025-09-26 16:56:23 +0100

    creé le model person

--- assurance/src/Models/Person.java
+++ assurance/src/Models/Person.java
@@ -1,4 +1,37 @@
 package Models;
 
 public class Person {
+    protected String nom;
+    protected String prenom;
+    protected String email;
+
+    public Person(String nom, String prenom, String email) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNom(String nom){
+        this.nom=nom;
+    }
+
+    public void setPrenom(String prenom){
+        this.prenom=prenom;
+    }
+
+    public void setEmail(String email){
+        this.email=email;
+    }
 }
