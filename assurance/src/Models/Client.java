62309eb5e0735c023d5cb33c7c099570d3304d22
asma828 <asmalachhab66@gmail.com>
2025-09-26 16:55:55 +0100

    creé le model client

--- assurance/src/Models/Client.java
+++ assurance/src/Models/Client.java
@@ -1,4 +1,43 @@
 package Models;
 
-public class Client {
+public class Client extends Person {
+    private int id;
+    private Conseiller conseiller;
+
+    public Client(int id ,String nom,String prenom,String email,Conseiller conseiller){
+        super(nom,prenom,email);
+        this.id=id;
+        this.conseiller=conseiller;
+    }
+
+    public Client(String nom, String prenom, String email, Conseiller conseiller) {
+        super(nom, prenom, email);
+        this.conseiller = conseiller;
+    }
+
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public Conseiller getConseiller(){
+        return conseiller;
+    }
+    public void setConseiller(Conseiller conseiller){
+        this.conseiller=conseiller;
+    }
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", nom='" + getNom() + '\'' +
+                ", prenom='" + getPrenom() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", conseiller=" + (conseiller != null ? conseiller.getNom() + " " + conseiller.getPrenom() : "null") +
+                '}';
+    }
 }
+
+
