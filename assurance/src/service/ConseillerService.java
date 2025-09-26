7c0b53a78b0d0775d1469dda0f097b32f1a61f55
asma828 <asmalachhab66@gmail.com>
2025-09-26 17:07:17 +0100

    creé conseiller Service

--- assurance/src/service/ConseillerService.java
+++ assurance/src/service/ConseillerService.java
@@ -1,4 +1,30 @@
 package service;
 
+import DAO.ClientDAO;
+import DAO.ConseillerDAO;
+import Models.Client;
+import Models.Conseiller;
+
+import java.util.List;
+import java.util.Optional;
+
 public class ConseillerService {
+    private ConseillerDAO conseillerDAO = new ConseillerDAO();
+    private ClientDAO clientDAO = new ClientDAO();
+
+    public int ajouterConseiller(Conseiller conseiller) {
+        return conseillerDAO.create(conseiller);
+    }
+
+    public boolean deleteConseiller(int id) {
+        return conseillerDAO.delete(id);
+    }
+
+    public Optional<Conseiller> rechercherConseillerParId(int id) {
+        return conseillerDAO.findById(id);
+    }
+
+    public List<Client> afficherClientsParConseillerId(int conseillerId) {
+        return clientDAO.findByConseillerId(conseillerId);
+    }
 }
