4e3847cc492152ce3c9893b611fd393cb4a288ea
asma828 <asmalachhab66@gmail.com>
2025-09-26 17:11:48 +0100

    creé contrat Service

--- assurance/src/service/ContratService.java
+++ assurance/src/service/ContratService.java
@@ -1,5 +1,27 @@
 package service;
 
-public class ContractService {
+import DAO.ContratDAO;
+import Models.Contrat;
 
+import java.util.List;
+import java.util.Optional;
+
+public class ContratService {
+    private ContratDAO contratDAO = new ContratDAO();
+
+    public int ajouterContrat(Contrat contrat) {
+        return contratDAO.create(contrat);
+    }
+
+    public boolean supprimerContrat(int id) {
+        return contratDAO.delete(id);
+    }
+
+    public Optional<Contrat> afficherContratParId(int id) {
+        return contratDAO.findById(id);
+    }
+
+    public List<Contrat> afficherContratsParClientId(int clientId) {
+        return contratDAO.findByClientId(clientId);
+    }
 }
