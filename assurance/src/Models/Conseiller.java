f1e8bae016935d9267be38bb9534d7719e1cf69e
asma828 <asmalachhab66@gmail.com>
2025-09-26 16:56:50 +0100

    creé le model conseiller

--- assurance/src/Models/Conseiller.java
+++ assurance/src/Models/Conseiller.java
@@ -1,4 +1,33 @@
 package Models;
 
-public class Conseiller {
+public class Conseiller extends Person {
+    private int id;
+
+    // Constructor  pour creé un nouveaux Conseiller
+    public Conseiller(String nom, String prenom, String email) {
+        super(nom, prenom, email);
+    }
+
+    // Constructeur utilisé lors de la lecture à partir de la base de données (avec id)
+    public Conseiller(int id, String nom, String prenom, String email) {
+        super(nom, prenom, email);
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Conseiller {" +
+                "id=" + id +
+                ", nom='" + getNom() + '\'' +
+                ", prenom='" + getPrenom() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                '}';
+    }
 }
